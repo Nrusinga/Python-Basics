09dcfd0962c1895801fcdc4e0a92cc0258ba70c7
Nrusinga <48976841+Nrusinga@users.noreply.github.com>
2024-03-06 00:47:34 -0700

    Update count.py
    Minor error

--- count.py
+++ count.py
@@ -1,15 +1,10 @@
 def count(x):
+    count=0
     while(x>0):
         x=x//10
         count+=1
     return count
 
 
-
-
-
-
-
-
-x= input("Enter the number:")
+x= int(input("Enter the number:"))
 print(count(x))
