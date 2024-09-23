e46a82ecf36e7d5b53e10d18809ab46dfac743fa
Nrusinga <gnrusinga@gmail.com>
2024-09-23 09:48:31 -0700

    Sqaure of nos

---
+++ sqauredofnos.py
@@ -0,0 +1,15 @@
+def squared_of_nos(numbers):
+    sqaurednos=[]
+    for i in numbers:
+        sqaurednos.append(i*i)
+
+    return sqaurednos
+
+
+
+
+
+
+numbers=[1,2,3,4]
+output=squared_of_nos(numbers)
+print(output)
